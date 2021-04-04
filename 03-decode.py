3870547cd088ec20d242d58fcad92158344d3b74
Rutger van Deelen <rutgervandeelen@innius.com>
2021-04-04 22:36:31 +0200

    Add dump file

--- 03-decode.py
+++ 03-decode.py
@@ -10,43 +10,49 @@ import base64
 import numpy as np
 from PIL import Image
 import collections
+import pprint
 
 path = pathlib.Path.cwd()
 input_filename = str(path / 'result' / 'result.txt')
 output_filename = str(path / 'result' / 'result.png')
 
-with open(input_filename, encoding='ascii') as reader:
+with open(input_filename, encoding='utf-8') as reader:
     text = reader.read()
-    print('Input text []:\n{}'.format(len(text), text))
+    print('Input text [{}]:\n{}'.format(len(text), text))
 
     c = collections.Counter(text)
-    print('Counter:\n{}', c)
+    print('Counter:')
+    pprint.pprint(c)
+    word = [k for k,v in c.items() if k.isalpha()]
+    print(word)
 
     # Cleanup
     text = text.replace("@", "c")
-    print('Input text cleaned []:\n{}'.format(len(text), text))
+    print('Input text cleaned [{}]:\n{}'.format(len(text), text))
 
     cc = collections.Counter(text)
-    print('Counter cleaned:\n{}', cc)
+    print('Counter cleaned:')
+    pprint.pprint(cc)
 
-    base64_bytes = text.encode('ascii')
+    word = [k for k,v in c.items() if k.isalpha()]
+    print(word)
+
+    base64_bytes = text.encode('utf-8')
 
     missing_padding = len(base64_bytes) % 4
     if missing_padding:
         base64_bytes += b'='* (4 - missing_padding)
 
-    # Remove the 4 padded bytes at end
-    base64_bytes = base64_bytes[:-4]
     print('Input bytes:\n{}'.format(base64_bytes))
 
-    arr = list(base64_bytes[:-4])
-    arr = np.asarray(arr, dtype=np.uint8)
-
     message_bytes = base64.b64decode(base64_bytes)
-    message = message_bytes.decode('ascii', 'replace')
+    message = message_bytes.decode('utf-8', 'replace')
     print('Decoded text:\n{}'.format(message))
 
-    # Is it perhaps an image?
-    # matrix = arr.reshape(200, 40)
-    # img = Image.fromarray(matrix)
-    # img.save(output_filename, "PNG")
+    arr = list(base64_bytes)
+    # print(arr)
+
+    arr = np.asarray(arr, dtype=np.uint8)
+    matrix = arr.reshape(8, 931)
+    img = Image.fromarray(matrix)
+    img.save(output_filename, "PNG")
